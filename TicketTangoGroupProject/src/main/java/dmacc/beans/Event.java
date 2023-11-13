65dddc152c61a9644ccfb0620ceea14c9cecca13
Eric Grabe <eric.grabe@gmail.com>
2023-11-13 16:41:10 -0600

    initial commit

---
+++ TicketTangoGroupProject/src/main/java/dmacc/beans/Event.java
@@ -0,0 +1,148 @@
+package dmacc.beans;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * @author Eric Grabe - egrabe
+ * CIS175 - Fall 2023
+ * Nov 12, 2023
+ */
+public class Event {
+	
+	private Long Id;
+	private LocalDate eventDate;
+	private String venueName;
+	private int ticketCount;
+	private String eventTitle;
+	private String performers;
+	private LocalTime startTime;
+	private LocalTime endTime;
+	
+	public Event() {
+		super();
+	}
+	
+	public Event(LocalDate eventDate, String venueName, int ticketCount, 
+					String eventTitle, String performers, 
+					LocalTime startTime, LocalTime endTime) {
+		this.eventDate = eventDate;
+		this.venueName = venueName;
+		this.ticketCount = ticketCount;
+		this.eventTitle = eventTitle;
+		this.performers = performers;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	
+	
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return Id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	/**
+	 * @return the eventDate
+	 */
+	public LocalDate getEventDate() {
+		return eventDate;
+	}
+	/**
+	 * @param eventDate the eventDate to set
+	 */
+	public void setEventDate(LocalDate eventDate) {
+		this.eventDate = eventDate;
+	}
+	/**
+	 * @return the venueName
+	 */
+	public String getVenueName() {
+		return venueName;
+	}
+	/**
+	 * @param venueName the venueName to set
+	 */
+	public void setVenueName(String venueName) {
+		this.venueName = venueName;
+	}
+	/**
+	 * @return the ticketCount
+	 */
+	public int getTicketCount() {
+		return ticketCount;
+	}
+	/**
+	 * @param ticketCount the ticketCount to set
+	 */
+	public void setTicketCount(int ticketCount) {
+		this.ticketCount = ticketCount;
+	}
+	/**
+	 * @return the eventTitle
+	 */
+	public String getEventTitle() {
+		return eventTitle;
+	}
+	/**
+	 * @param eventTitle the eventTitle to set
+	 */
+	public void setEventTitle(String eventTitle) {
+		this.eventTitle = eventTitle;
+	}
+	/**
+	 * @return the performers
+	 */
+	public String getPerformers() {
+		return performers;
+	}
+	/**
+	 * @param performers the performers to set
+	 */
+	public void setPerformers(String performers) {
+		this.performers = performers;
+	}
+	/**
+	 * @return the startTime
+	 */
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+	/**
+	 * @return the endTime
+	 */
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+	@Override
+	public String toString() {
+		return "Event [eventDate=" + eventDate + ", venueName=" + venueName + ", ticketCount=" + ticketCount
+				+ ", eventTitle=" + eventTitle + ", performers=" + performers + ", startTime=" + startTime
+				+ ", endTime=" + endTime + "]";
+	}
+	
+	
+	
+
+}
